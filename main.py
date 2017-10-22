739a02c1aa618becddb4f0f83993b8ba93ea4986
Artem Lagzdin <art.lagzdin@gmail.com>
2017-10-22 13:52:32 +0600

    UX improvements.

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def load_vgg(sess, vgg_path):
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+    # Implement function
+    # Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
@@ -55,7 +55,7 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
+    # Implement function
     layer_conv1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3), name='layer_conv1x1')
 
@@ -86,7 +86,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
+    # Implement function
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
@@ -102,6 +102,7 @@ def save_model(sess, saver, save_path):
     :param save_path: Path to save the model
     """
     saver.save(sess, save_path)
+    print('Model saved to {}'.format(save_path))
 
 def restore_model(sess, saver, save_path):
     """
@@ -111,6 +112,7 @@ def restore_model(sess, saver, save_path):
     :param save_path: Path to save the model
     """
     saver.restore(sess, save_path)
+    print('Model restored from {}'.format(save_path))
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate, saver=None, save_path=None):
@@ -210,7 +212,7 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+        # Build NN using load_vgg, layers, and optimize function
         input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
         nn_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
         logits, train_op, cross_entropy_loss = optimize(nn_output, correct_label, learning_rate, num_classes)
@@ -218,16 +220,17 @@ def run():
         saver = tf.train.Saver()
         sess.run(tf.global_variables_initializer())
 
-        # TODO: Train NN using the train_nn function
+        # Train NN using the train_nn function
         if load_model:
             restore_model(sess, saver, save_path)
-        else:
+
+        if epochs > 0:
             train_nn(sess, epochs, batch_size, get_batches_fn,
                 train_op, cross_entropy_loss, input_image,
                 correct_label, keep_prob, learning_rate,
                 saver, save_path)
 
-        # TODO: Save inference data using helper.save_inference_samples
+        # Save inference data using helper.save_inference_samples
         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
